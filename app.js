7badeb66e252ef9273f998d60f977150358a6cc8
salmanshahid5 <salmanshahid0333@gmail.com>
2024-07-21 16:01:49 +0500

    bss timer add krna hai

--- app.js
+++ app.js
@@ -56,17 +56,40 @@ const quizData = [
 
 // varible initialization
 var questionText = document.getElementById('question');
+const quiz = document.getElementById('quiz')
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submit = document.getElementById('submit');
+const timerElement = document.getElementById('timer');
 
 var index = 0;
+var score = 0;
+var timer;
+
+function startTimer(){
+    var timeLeft = 59;
+    timerElement.innerHTML = `Time Left: ${timeLeft}s`;
+
+    timer = setInterval(()=>{
+        timeLeft--;
+        timerElement.innerHTML = `Time Left: ${timeLeft}s`;
+
+        if(timeLeft <= 0){
+            clearInterval(timer);
+            NextQuestion()
+        }
+    },1000)
+}
 
 function NextQuestion(){
     if(index > quizData.length-1){
-        console.log('The end');
+        quiz.innerHTML = `
+        <h2>You answered ${score +1}/${quizData.length} questions correctly</h2>
+        <h3>Your score: ${(score  / quizData.length * 100).toFixed(2)}%</h3>`;
+        clearInterval(timer);
+        return
     }
     else{
     questionText.innerHTML = quizData[index].question;
@@ -80,9 +103,10 @@ function NextQuestion(){
     for (var i = 0; i < answer.length; i++) {
         if(answer[i].checked){
             var userSelect = answer[i].id;
-            var correctAns = quizData[index -1].correct;
-            console.log(userSelect);
-            console.log(correctAns);
+            var correctAns = quizData[index-2].correct;
+            if(userSelect == correctAns){
+                score++;
+            }
         }
         answer[i].checked = false;
         submit.disabled = true;
