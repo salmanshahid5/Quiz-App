8f61d8b0f5ba231c5bb85b2b4f795cb16d092529
salmanshahid5 <salmanshahid0333@gmail.com>
2024-07-22 22:03:41 +0500

    result show kr wa diya

--- app.js
+++ app.js
@@ -68,53 +68,78 @@ var index = 0;
 var score = 0;
 var timer;
 
-function startTimer(){
-    var timeLeft = 59;
+function startTimer() {
+    var timeLeft = 295;
     timerElement.innerHTML = `Time Left: ${timeLeft}s`;
 
-    timer = setInterval(()=>{
+    timer = setInterval(() => {
         timeLeft--;
         timerElement.innerHTML = `Time Left: ${timeLeft}s`;
 
-        if(timeLeft <= 0){
+        if (timeLeft <= 0) {
             clearInterval(timer);
-            NextQuestion()
+            NextQuestion();
         }
-    },1000)
+    }, 1000)
 }
 
-function NextQuestion(){
-    if(index > quizData.length-1){
-        quiz.innerHTML = `
-        <h2>You answered ${score +1}/${quizData.length} questions correctly</h2>
-        <h3>Your score: ${(score  / quizData.length * 100).toFixed(2)}%</h3>`;
+function NextQuestion() {
+    if (index > quizData.length - 1) {
+        let message, messageClass;
+        if ((score+1) / quizData.length >= 0.5) {
+            message = 'Congratulations, you passed';
+            messageClass = 'green';
+        }
+        else {
+            message = 'Sorry, you failed';
+            messageClass = 'red';
+        }
+        quiz.innerHTML = `<div class="container">
+    <p class="congratulations ${messageClass}">${message}</p>
+    <div class="results">
+        <div class="total-questions" style="border-bottom: 1px solid grey;">
+            <span>Total Questions</span>
+            <span>${quizData.length}</span>
+        </div>
+        <div class="correct-questions">
+            <span>Correct Questions</span>
+            <span>${(score+1)}</span>
+        </div>
+        <div class="score-circle">
+            <div class="circle">
+                <span>${((score+1) / quizData.length * 100).toFixed(1)}%</span>
+            </div>
+        </div>
+    </div>
+    <button class="back-to-home" onclick="location.reload()">Back to home</button>
+</div>`
         clearInterval(timer);
-        return
+        return;
     }
-    else{
-    questionText.innerHTML = quizData[index].question;
-    a_text.innerText = quizData[index].a;
-    b_text.innerText = quizData[index].b;
-    c_text.innerText = quizData[index].c;
-    d_text.innerText = quizData[index].d;
-    index++;
+    else {
+        questionText.innerHTML = quizData[index].question;
+        a_text.innerText = quizData[index].a;
+        b_text.innerText = quizData[index].b;
+        c_text.innerText = quizData[index].c;
+        d_text.innerText = quizData[index].d;
+        index++;
     }
     const answer = document.getElementsByName('answer');
     for (var i = 0; i < answer.length; i++) {
-        if(answer[i].checked){
+        if (answer[i].checked) {
             var userSelect = answer[i].id;
-            var correctAns = quizData[index-2].correct;
-            if(userSelect == correctAns){
+            var correctAns = quizData[index - 2].correct;
+            if (userSelect == correctAns) {
                 score++;
             }
         }
         answer[i].checked = false;
         submit.disabled = true;
-    } 
+    }
 }
-
+startTimer()
 NextQuestion();
 
-function enabled(){
+function enabled() {
     submit.disabled = false;
 }
